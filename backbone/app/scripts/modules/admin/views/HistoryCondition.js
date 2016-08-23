392d8da7420595f768b2a06af06333094b16b401
No60 <masashi_kobari@pressman.ne.jp>
2016-08-23 12:58:00 +0900

    SUNPEXIST_UNIVERSAL-14 フロント側修正、発注状況照会.css追加

--- backbone/app/scripts/modules/admin/views/HistoryCondition.js
+++ backbone/app/scripts/modules/admin/views/HistoryCondition.js
@@ -27,9 +27,11 @@ define([
 			ui: {
 				'agreement_no': '#agreement_no',
 				'no': '#no',
+				'emply_order_no': '#emply_order_no',
 				'member_no': '#member_no',
-				'office': '#office',
-				'office_cd': '#office_cd',
+				'member_name': '#member_name',
+//				'office': '#office',
+//				'office_cd': '#office_cd',
 				'section': '#section',
 				'job_type': '#job_type',
 				"input_item": "#input_item",
@@ -37,6 +39,8 @@ define([
 				"item_size": "#item_size",
 				'order_day_from': '#order_day_from',
 				'order_day_to': '#order_day_to',
+				'send_day_from': '#send_day_from',
+				'send_day_to': '#send_day_to',
 				'status0': '#status0',
 				'status1': '#status1',
 //				'status2': '#status2',
@@ -47,26 +51,26 @@ define([
 				'order_kbn2': '#order_kbn2',
 				'order_kbn3': '#order_kbn3',
 				'order_kbn4': '#order_kbn4',
-				'order_reason_kbn0': '#order_reason_kbn0',
-				'order_reason_kbn1': '#order_reason_kbn1',
-				'order_reason_kbn2': '#order_reason_kbn2',
-				'order_reason_kbn3': '#order_reason_kbn3',
-				'order_reason_kbn4': '#order_reason_kbn4',
-				'order_reason_kbn5': '#order_reason_kbn5',
-				'order_reason_kbn6': '#order_reason_kbn6',
-				'order_reason_kbn7': '#order_reason_kbn7',
-				'order_reason_kbn8': '#order_reason_kbn8',
-				'order_reason_kbn9': '#order_reason_kbn9',
-				'order_reason_kbn10': '#order_reason_kbn10',
-				'order_reason_kbn11': '#order_reason_kbn11',
-				'order_reason_kbn12': '#order_reason_kbn12',
-				'order_reason_kbn13': '#order_reason_kbn13',
-				'order_reason_kbn14': '#order_reason_kbn14',
-				'order_reason_kbn15': '#order_reason_kbn15',
-				'order_reason_kbn16': '#order_reason_kbn16',
-				'order_reason_kbn17': '#order_reason_kbn17',
-				'order_reason_kbn18': '#order_reason_kbn18',
-				'order_reason_kbn19': '#order_reason_kbn19',
+				'reason_kbn0': '#reason_kbn0',
+				'reason_kbn1': '#reason_kbn1',
+				'reason_kbn2': '#reason_kbn2',
+				'reason_kbn3': '#reason_kbn3',
+				'reason_kbn4': '#reason_kbn4',
+				'reason_kbn5': '#reason_kbn5',
+				'reason_kbn6': '#reason_kbn6',
+				'reason_kbn7': '#reason_kbn7',
+				'reason_kbn8': '#reason_kbn8',
+				'reason_kbn9': '#reason_kbn9',
+				'reason_kbn10': '#reason_kbn10',
+				'reason_kbn11': '#reason_kbn11',
+				'reason_kbn12': '#reason_kbn12',
+				'reason_kbn13': '#reason_kbn13',
+				'reason_kbn14': '#reason_kbn14',
+				'reason_kbn15': '#reason_kbn15',
+				'reason_kbn16': '#reason_kbn16',
+				'reason_kbn17': '#reason_kbn17',
+				'reason_kbn18': '#reason_kbn18',
+				'reason_kbn19': '#reason_kbn19',
 				"individual_number": "#individual_number",
 				"search": '.search',
 				'datepicker': '.datepicker',
@@ -75,9 +79,11 @@ define([
 			bindings: {
 				'#agreement_no': 'agreement_no',
 				'#no': 'no',
+				'#emply_order_no': 'emply_order_no',
 				'#member_no': 'member_no',
-				'#office': 'office',
-				'#office_cd': 'office_cd',
+				'#member_name': 'member_name',
+//				'#office': 'office',
+//				'#office_cd': 'office_cd',
 				'#section': 'section',
 				'#job_type': 'job_type',
 				"#input_item": "input_item",
@@ -85,6 +91,8 @@ define([
 				"#item_size": "item_size",
 				'#order_day_from': 'order_day_from',
 				'#order_day_to': 'order_day_to',
+				'#send_day_from': 'send_day_from',
+				'#send_day_to': 'send_day_to',
 				'#status0': 'status0',
 				'#status1': 'status1',
 //				'#status2': 'status2',
@@ -95,26 +103,26 @@ define([
 				'#order_kbn2': 'order_kbn2',
 				'#order_kbn3': 'order_kbn3',
 				'#order_kbn4': 'order_kbn4',
-				'#order_reason_kbn0': 'order_reason_kbn0',
-				'#order_reason_kbn1': 'order_reason_kbn1',
-				'#order_reason_kbn2': 'order_reason_kbn2',
-				'#order_reason_kbn3': 'order_reason_kbn3',
-				'#order_reason_kbn4': 'order_reason_kbn4',
-				'#order_reason_kbn5': 'order_reason_kbn5',
-				'#order_reason_kbn6': 'order_reason_kbn6',
-				'#order_reason_kbn7': 'order_reason_kbn7',
-				'#order_reason_kbn8': 'order_reason_kbn8',
-				'#order_reason_kbn9': 'order_reason_kbn9',
-				'#order_reason_kbn10': 'order_reason_kbn10',
-				'#order_reason_kbn11': 'order_reason_kbn11',
-				'#order_reason_kbn12': 'order_reason_kbn12',
-				'#order_reason_kbn13': 'order_reason_kbn13',
-				'#order_reason_kbn14': 'order_reason_kbn14',
-				'#order_reason_kbn15': 'order_reason_kbn15',
-				'#order_reason_kbn16': 'order_reason_kbn16',
-				'#order_reason_kbn17': 'order_reason_kbn17',
-				'#order_reason_kbn18': 'order_reason_kbn18',
-				'#order_reason_kbn19': 'order_reason_kbn19',
+				'#reason_kbn0': 'reason_kbn0',
+				'#reason_kbn1': 'reason_kbn1',
+				'#reason_kbn2': 'reason_kbn2',
+				'#reason_kbn3': 'reason_kbn3',
+				'#reason_kbn4': 'reason_kbn4',
+				'#reason_kbn5': 'reason_kbn5',
+				'#reason_kbn6': 'reason_kbn6',
+				'#reason_kbn7': 'reason_kbn7',
+				'#reason_kbn8': 'reason_kbn8',
+				'#reason_kbn9': 'reason_kbn9',
+				'#reason_kbn10': 'reason_kbn10',
+				'#reason_kbn11': 'reason_kbn11',
+				'#reason_kbn12': 'reason_kbn12',
+				'#reason_kbn13': 'reason_kbn13',
+				'#reason_kbn14': 'reason_kbn14',
+				'#reason_kbn15': 'reason_kbn15',
+				'#reason_kbn16': 'reason_kbn16',
+				'#reason_kbn17': 'reason_kbn17',
+				'#reason_kbn18': 'reason_kbn18',
+				'#reason_kbn19': 'reason_kbn19',
 				"#individual_number": "individual_number",
 				'#search': 'search',
 				'#datepicker': 'datepicker',
@@ -206,6 +214,7 @@ define([
 					}
 					//local:[{text:'a'},{text:'b'},{text:'ab'},{text:'abc def'}]
 				});
+/*
 				var target = this.ui.office;
 				target.typeahead({
 					//highlight:false,
@@ -227,6 +236,7 @@ define([
 				});
 				// 拠点コードを要素に代入
 				this.ui.office_cd = $office_cd;
+*/
 			},
 		events: {
 			'click @ui.search': function(e){
@@ -234,9 +244,11 @@ define([
 				this.triggerMethod('hideAlerts');
 				this.model.set('agreement_no', this.ui.agreement_no.val());
 				this.model.set('no', this.ui.no.val());
+				this.model.set('emply_order_no', this.ui.emply_order_no.val());
 				this.model.set('member_no', this.ui.member_no.val());
-				this.model.set('office', this.ui.office.val());
-				this.model.set('office_cd', this.ui.office_cd.val());
+				this.model.set('member_name', this.ui.member_name.val());
+//				this.model.set('office', this.ui.office.val());
+//				this.model.set('office_cd', this.ui.office_cd.val());
 				this.model.set('section', this.ui.section.val());
 				this.model.set('job_type', this.ui.job_type.val());
 				this.model.set('input_item', this.ui.input_item.val());
@@ -244,6 +256,8 @@ define([
 				this.model.set('item_size', this.ui.item_size.val());
 				this.model.set('order_day_from', this.ui.order_day_from.val());
 				this.model.set('order_day_to', this.ui.order_day_to.val());
+				this.model.set('send_day_from', this.ui.send_day_from.val());
+				this.model.set('send_day_to', this.ui.send_day_to.val());
 				this.model.set('status0', this.ui.status0.prop('checked'));
 				this.model.set('status1', this.ui.status1.prop('checked'));
 //				this.model.set('status2', this.ui.status2.prop('checked'));
@@ -254,26 +268,26 @@ define([
 				this.model.set('order_kbn2', this.ui.order_kbn2.prop('checked'));
 				this.model.set('order_kbn3', this.ui.order_kbn3.prop('checked'));
 				this.model.set('order_kbn4', this.ui.order_kbn4.prop('checked'));
-				this.model.set('order_reason_kbn0', this.ui.order_reason_kbn0.prop('checked'));
-				this.model.set('order_reason_kbn1', this.ui.order_reason_kbn1.prop('checked'));
-				this.model.set('order_reason_kbn2', this.ui.order_reason_kbn2.prop('checked'));
-				this.model.set('order_reason_kbn3', this.ui.order_reason_kbn3.prop('checked'));
-				this.model.set('order_reason_kbn4', this.ui.order_reason_kbn4.prop('checked'));
-				this.model.set('order_reason_kbn5', this.ui.order_reason_kbn5.prop('checked'));
-				this.model.set('order_reason_kbn6', this.ui.order_reason_kbn6.prop('checked'));
-				this.model.set('order_reason_kbn7', this.ui.order_reason_kbn7.prop('checked'));
-				this.model.set('order_reason_kbn8', this.ui.order_reason_kbn8.prop('checked'));
-				this.model.set('order_reason_kbn9', this.ui.order_reason_kbn9.prop('checked'));
-				this.model.set('order_reason_kbn10', this.ui.order_reason_kbn10.prop('checked'));
-				this.model.set('order_reason_kbn11', this.ui.order_reason_kbn11.prop('checked'));
-				this.model.set('order_reason_kbn12', this.ui.order_reason_kbn12.prop('checked'));
-				this.model.set('order_reason_kbn13', this.ui.order_reason_kbn13.prop('checked'));
-				this.model.set('order_reason_kbn14', this.ui.order_reason_kbn14.prop('checked'));
-				this.model.set('order_reason_kbn15', this.ui.order_reason_kbn15.prop('checked'));
-				this.model.set('order_reason_kbn16', this.ui.order_reason_kbn16.prop('checked'));
-				this.model.set('order_reason_kbn17', this.ui.order_reason_kbn17.prop('checked'));
-				this.model.set('order_reason_kbn18', this.ui.order_reason_kbn18.prop('checked'));
-				this.model.set('order_reason_kbn19', this.ui.order_reason_kbn19.prop('checked'));
+				this.model.set('reason_kbn0', this.ui.reason_kbn0.prop('checked'));
+				this.model.set('reason_kbn1', this.ui.reason_kbn1.prop('checked'));
+				this.model.set('reason_kbn2', this.ui.reason_kbn2.prop('checked'));
+				this.model.set('reason_kbn3', this.ui.reason_kbn3.prop('checked'));
+				this.model.set('reason_kbn4', this.ui.reason_kbn4.prop('checked'));
+				this.model.set('reason_kbn5', this.ui.reason_kbn5.prop('checked'));
+				this.model.set('reason_kbn6', this.ui.reason_kbn6.prop('checked'));
+				this.model.set('reason_kbn7', this.ui.reason_kbn7.prop('checked'));
+				this.model.set('reason_kbn8', this.ui.reason_kbn8.prop('checked'));
+				this.model.set('reason_kbn9', this.ui.reason_kbn9.prop('checked'));
+				this.model.set('reason_kbn10', this.ui.reason_kbn10.prop('checked'));
+				this.model.set('reason_kbn11', this.ui.reason_kbn11.prop('checked'));
+				this.model.set('reason_kbn12', this.ui.reason_kbn12.prop('checked'));
+				this.model.set('reason_kbn13', this.ui.reason_kbn13.prop('checked'));
+				this.model.set('reason_kbn14', this.ui.reason_kbn14.prop('checked'));
+				this.model.set('reason_kbn15', this.ui.reason_kbn15.prop('checked'));
+				this.model.set('reason_kbn16', this.ui.reason_kbn16.prop('checked'));
+				this.model.set('reason_kbn17', this.ui.reason_kbn17.prop('checked'));
+				this.model.set('reason_kbn18', this.ui.reason_kbn18.prop('checked'));
+				this.model.set('reason_kbn19', this.ui.reason_kbn19.prop('checked'));
 				this.model.set('individual_number', this.ui.individual_number.val());
 				this.model.set('search', this.ui.search.val());
 				this.model.set('datepicker', this.ui.datepicker.val());
