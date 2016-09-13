d72c2263a680039727b5745a9320fc437345e317
m.maruyama <masahiro_maruyama@pressman.ne.jp>
2016-09-13 18:50:29 +0900

    SUNPEXIST_UNIVERSAL-27 アカウント管理

--- app/account.php
+++ app/account.php
@@ -1,5 +1,8 @@
 <?php
-
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Resultset\Simple as Resultset;
+use Phalcon\Paginator\Adapter\Model as PaginatorModel;
+use Phalcon\Paginator\Adapter\NativeArray as PaginatorArray;
 use Phalcon\Paginator\Adapter\QueryBuilder as PaginatorQueryBuilder;
 
 /*
