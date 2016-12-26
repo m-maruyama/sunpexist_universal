460d5193705ccffe0b8a3a54887deced437a0220
TetsuNakamura <tetsu_nakamura@pressman.ne.jp>
2016-12-26 11:02:01 +0900

    【貸与終了】ゼロ埋め拠点修正

--- app/wearer_end.php
+++ app/wearer_end.php
@@ -54,7 +54,7 @@ $app->post('/wearer_end/search', function ()use($app){
             $all_list['a'][] = $result->rntl_sect_cd;
         }
     }
-    if (in_array("0000000000", $all_list)) {
+    if (in_array("0000000000", $all_list['a'])) {
         $section_all_zero_flg = true;
     } else {
         $section_all_zero_flg = false;
@@ -84,7 +84,7 @@ $app->post('/wearer_end/search', function ()use($app){
     if (!$section_all_zero_flg) {
         $query_list[] = "wcr.corporate_id = '".$auth['corporate_id']."'";
         $query_list[] = "wcr.rntl_cont_no = '".$cond['agreement_no']."'";
-        //$query_list[] = "wcr.accnt_no = '".$auth['accnt_no']."'";
+        $query_list[] = "wcr.accnt_no = '".$auth['accnt_no']."'";
     }
     $query = implode(' AND ', $query_list);
 
