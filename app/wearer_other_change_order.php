f4b52c4230ea5883c93a1e418fdbdf6ab1c66db7
m.maruyama <masahiro_maruyama@pressman.ne.jp>
2016-12-26 15:11:19 +0900

    変数修正

--- app/wearer_other_change_order.php
+++ app/wearer_other_change_order.php
@@ -778,7 +778,7 @@ $app->post('/wearer_other_change_list', function ()use($app){
             foreach ($item_results as $item_result) {
                 // 交換可能枚数
                 if (individual_flg($auth['corporate_id'], $wearer_size_change_post['rntl_cont_no']) == "1") {
-                    $list["rtn_ok_cnt"] = $result->as_std_input_qty;
+                    $list["rtn_ok_cnt"] = $item_result->as_std_input_qty;
                 }else{
                     //数量 - 返却予定数が0以下だったら表示しない
                     if(($item_result->as_quantity - $item_result->as_return_plan_qty) <= 0){
