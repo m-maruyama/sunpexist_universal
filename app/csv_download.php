243af2d4c825c9c728eb7b82708078a86b4b85c2
m.maruyama <masahiro_maruyama@pressman.ne.jp>
2017-01-12 19:15:45 +0900

    SUNPEXIST_UNIVERSAL-310 【照会画面：ダウンロード】ソート条件を渡せていない

--- app/csv_download.php
+++ app/csv_download.php
@@ -1399,6 +1399,7 @@ $app->post('/csv_download', function ()use($app){
             $arg_str .= "t_order.job_type_cd as as_job_type_cd,";
             $arg_str .= "t_order.size_two_cd as as_size_two_cd,";
             $arg_str .= "t_order.order_qty as as_order_qty,";
+            $arg_str .= "m_input_item.input_item_name as as_input_item_name,";
             $arg_str .= "t_returned_plan_info.order_date as as_re_order_date,";
             $arg_str .= "t_returned_plan_info.return_status as as_return_status,";
             $arg_str .= "t_returned_plan_info.return_date as as_return_date,";
@@ -1439,8 +1440,16 @@ $app->post('/csv_download', function ()use($app){
                 $arg_str .= " AND m_section.rntl_sect_cd = m_contract_resource.rntl_sect_cd";
                 $arg_str .= " ) ON t_order.m_section_comb_hkey = m_section.m_section_comb_hkey";
             }
-            $arg_str .= " INNER JOIN m_job_type";
-            $arg_str .= " ON t_order.m_job_type_comb_hkey = m_job_type.m_job_type_comb_hkey";
+            $arg_str .= " LEFT JOIN (m_job_type INNER JOIN m_input_item";
+            $arg_str .= " ON m_job_type.corporate_id = m_input_item.corporate_id";
+            $arg_str .= " AND m_job_type.rntl_cont_no = m_input_item.rntl_cont_no";
+            $arg_str .= " AND m_job_type.job_type_cd = m_input_item.job_type_cd)";
+            $arg_str .= " ON t_order.corporate_id = m_job_type.corporate_id";
+            $arg_str .= " AND t_order.rntl_cont_no = m_job_type.rntl_cont_no";
+            $arg_str .= " AND t_order.job_type_cd = m_job_type.job_type_cd";
+            $arg_str .= " AND t_order.corporate_id = m_input_item.corporate_id";
+            $arg_str .= " AND t_order.item_cd = m_input_item.item_cd";
+            $arg_str .= " AND t_order.color_cd = m_input_item.color_cd";
             $arg_str .= " INNER JOIN m_wearer_std";
             $arg_str .= " ON t_order.werer_cd = m_wearer_std.werer_cd";
             $arg_str .= " AND t_order.corporate_id = m_wearer_std.corporate_id";
@@ -1504,8 +1513,16 @@ $app->post('/csv_download', function ()use($app){
                 $arg_str .= " AND m_section.rntl_sect_cd = m_contract_resource.rntl_sect_cd";
                 $arg_str .= " ) ON t_order.m_section_comb_hkey = m_section.m_section_comb_hkey";
             }
-            $arg_str .= " INNER JOIN m_job_type";
-            $arg_str .= " ON t_order.m_job_type_comb_hkey = m_job_type.m_job_type_comb_hkey";
+            $arg_str .= " LEFT JOIN (m_job_type INNER JOIN m_input_item";
+            $arg_str .= " ON m_job_type.corporate_id = m_input_item.corporate_id";
+            $arg_str .= " AND m_job_type.rntl_cont_no = m_input_item.rntl_cont_no";
+            $arg_str .= " AND m_job_type.job_type_cd = m_input_item.job_type_cd)";
+            $arg_str .= " ON t_order.corporate_id = m_job_type.corporate_id";
+            $arg_str .= " AND t_order.rntl_cont_no = m_job_type.rntl_cont_no";
+            $arg_str .= " AND t_order.job_type_cd = m_job_type.job_type_cd";
+            $arg_str .= " AND t_order.corporate_id = m_input_item.corporate_id";
+            $arg_str .= " AND t_order.item_cd = m_input_item.item_cd";
+            $arg_str .= " AND t_order.color_cd = m_input_item.color_cd";
             $arg_str .= " INNER JOIN m_wearer_std";
             $arg_str .= " ON t_order.werer_cd = m_wearer_std.werer_cd";
             $arg_str .= " AND t_order.corporate_id = m_wearer_std.corporate_id";
