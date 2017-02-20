524d66dffd50d1a29540176a1ccf15b290396fb5
TetsuNakamura <tetsu_nakamura@pressman.ne.jp>
2017-02-20 19:53:23 +0900

    着用者詳細のサイズ２トリムしてから空かチェック

--- app/wearer.php
+++ app/wearer.php
@@ -746,9 +746,10 @@ $app->post('/wearer/detail', function () use ($app) {
         array_push($search_q, "item_cd = '" . $list['item_cd'] . "'");
         array_push($search_q, "color_cd = '" . $list['color_cd'] . "'");
         //サイズ2が空だったらサイズ2を検索条件に入れない
-        //if($list['size_two_cd'] !== '') {
-        //    array_push($search_q, "size_two_cd = '".$list['size_two_cd']."'");
-        //}
+          $list['size_two_cd'] = trim(mb_convert_kana( $list['size_two_cd'], "s"));
+          if($list['size_two_cd'] !== '') {
+              array_push($search_q, "size_two_cd = '".$list['size_two_cd']."'");
+          }
         //sql文字列を' AND 'で結合
         $query = implode(' AND ', $search_q);
         $input_item = MInputItem::query()
@@ -1006,7 +1007,7 @@ $app->post('/wearer/detail', function () use ($app) {
         array_push($search_q, "color_cd = '" . $list['color_cd'] . "'");
         //サイズ2が空だったらサイズ2を検索条件に入れない
 //        if ($list['size_two_cd'] !== '') {
-//          array_push($search_q, "size_two_cd = '" . $list['size_two_cd'] . "'");
+//            array_push($search_q, "size_two_cd = '" . $list['size_two_cd'] . "'");
 //        }
         //sql文字列を' AND 'で結合
         $query = implode(' AND ', $search_q);
