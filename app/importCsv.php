de2e8ffe3ce99de96f990c03acb826f2dcc0a570
TetsuNakamura <tetsu_nakamura@pressman.ne.jp>
2017-02-24 15:38:41 +0900

    CSVインポート

--- app/importCsv.php
+++ app/importCsv.php
@@ -1628,18 +1628,18 @@ function chk_format($error_list, $line_list, $line_cnt)
             array_push($error_list, error_msg_format($line_cnt, '社員番号'));
         }
     }
-    if (!empty($line_list[1])) {
-        //着用者名
-        if (!chk_pattern($line_list[1], 13)) {
-            array_push($error_list, error_msg_format($line_cnt, '着用者名'));
-        }
-    }
-    if (!empty($line_list[2])) {
-        //着用者名（カナ）
-        if (!chk_pattern($line_list[2], 14)) {
-            array_push($error_list, error_msg_format($line_cnt, '着用者名（カナ）'));
-        }
-    }
+//    if (!empty($line_list[1])) {
+//        //着用者名
+//        if (!chk_pattern($line_list[1], 13)) {
+//            array_push($error_list, error_msg_format($line_cnt, '着用者名'));
+//        }
+//    }
+//    if (!empty($line_list[2])) {
+//        //着用者名（カナ）
+//        if (!chk_pattern($line_list[2], 14)) {
+//            array_push($error_list, error_msg_format($line_cnt, '着用者名（カナ）'));
+//        }
+//    }
     if (!empty($line_list[3])) {
         //性別区分
         if (!chk_pattern($line_list[3], 2)) {
@@ -1832,22 +1832,22 @@ function chk_pattern($val, $pattaern)
                 return true;
             }
             break;
-        case 13:
-            //パターン13(22byte) //着用者名
-            if (strlen(mb_convert_encoding($val, 'SJIS', 'UTF-8')) > 22) {
-                return false;
-            } else {
-                return true;
-            }
-            break;
-        case 14:
-            //パターン14(25byte)1か2  //着用者名（カナ）
-            if (strlen(mb_convert_encoding($val, 'SJIS', 'UTF-8')) > 25) {
-                return false;
-            } else {
-                return true;
-            }
-            break;
+//        case 13:
+//            //パターン13(22byte) //着用者名
+//            if (strlen(mb_convert_encoding($val, 'SJIS', 'UTF-8')) > 22) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//            break;
+//        case 14:
+//            //パターン14(25byte)1か2  //着用者名（カナ）
+//            if (strlen(mb_convert_encoding($val, 'SJIS', 'UTF-8')) > 25) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//            break;
 
 
         default:
@@ -2078,9 +2078,9 @@ function input_check($line_list, $line_cnt)
 
     //日本語文字数チェック
     //着用者漢字
-    if (byte_cnv($line_list[1]) > 100) {
+    if (byte_cnv($line_list[1]) > 22) {
         if (count($error_list) < 20) {
-            $error_list[] = '着用者名の文字数が多すぎます。';
+            $error_list[] = '着用者名の文字数が多すぎます。（最大全角11文字）';
         } else {
             $json_list['errors'] = $error_list;
             $json_list["error_code"] = "1";
@@ -2089,9 +2089,9 @@ function input_check($line_list, $line_cnt)
         }
     }
     //着用者カナ
-    if (byte_cnv($line_list[2]) > 100) {
+    if (byte_cnv($line_list[2]) > 25) {
         if (count($error_list) < 20) {
-            $error_list[] = '着用者名(カナ)の文字数が多すぎます。';
+            $error_list[] = '着用者名(カナ)の文字数が多すぎます。（最大全角12文字）';
         } else {
             $json_list['errors'] = $error_list;
             $json_list["error_code"] = "1";
@@ -2102,7 +2102,7 @@ function input_check($line_list, $line_cnt)
     //伝言
     if (byte_cnv($line_list[14]) > 100) {
         if (count($error_list) < 20) {
-            $error_list[] = '伝言欄の文字数が多すぎます。';
+            $error_list[] = '伝言欄の文字数が多すぎます。（最大全角50文字）';
         } else {
             $json_list['errors'] = $error_list;
             $json_list["error_code"] = "1";
